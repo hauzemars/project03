ee9692fd0df96b9f694a87d89ab0462f326f578b
hauzemars <hzpart3@gmail.com>
2022-08-07 17:18:31 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Dinner from './dinner';
+
+
+function App() {
+  return (
+  <div className="App">
+        <Dinner dishName="Beef Biryani" sweetDish="Kheer"/>
+  </div>
+  );
+  
+}
+
+export default App;
+
---
+++ src/dinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dinner(abc){
+return(
+    <div>
+
+        <h1>Today we are serving {abc.dishName}</h1>
+        <h1>Today we are serving {abc.sweetDish}</h1>
+    </div>
+)
+
+}
+
+export default Dinner;
